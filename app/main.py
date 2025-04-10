4e91b56a10401d3bb99d19c4e63b6ba8ca9cc04b
RomanSapunGit <sapunroma23@gmail.com>
2025-04-10 16:15:39 +0200

    Develop solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,34 @@
 class Person:
-    # write your code here
-    pass
+    people: dict = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result_list = [Person(name=person["name"], age=person["age"])
+                   for person in people]
+    for index, person in enumerate(result_list):
+        spouse = people[index]
+        if spouse.get("wife") is not None:
+            wife = result_list[
+                find_person_by_name(person.name, result_list)
+            ]
+            person.wife = wife
+            wife.husband = person
+        elif spouse.get("husband") is not None:
+            husband = result_list[
+                find_person_by_name(person.name, result_list)
+            ]
+            person.husband = husband
+            husband.wife = person
+    return result_list
+
+
+def find_person_by_name(name: str, people: list) -> int:
+    for index in range(len(people)):
+        if people[index].name == name:
+            return index
+    return -1
